d07e3d153d56a12cddd7c9fedc7bc07e4cb04cef
dave-2112 <davemesfin16@gmail.com>
2022-06-22 17:23:47 +0300

    file regstration

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="middlename"
+            :rules="nameRules"
+            :counter="10"
+            label="middle name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+
+         <v-btn
+        class="mr-4"
+        type="submit"
+        :disabled="invalid"
+      >
+        submit
+      </v-btn>
+      <v-btn @click="clear">
+        clear
+      </v-btn>
+    
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      firstname: '',
+      middlename: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+    }),
+    methods: {
+      submit () {
+        this.$refs.observer.validate()
+      },
+      clear () {
+        this.name = ''
+        this.middlename = ''
+        this.lastname = ''
+        this.email = ''
+        this.$refs.observer.reset()
+      },
+    },
+  }
+</script>
